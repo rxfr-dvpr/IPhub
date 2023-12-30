15f10b1bbd568901b19262c6d61d9d4c34317ee6
RXFR <freelanceredfox@gmail.com>
2023-12-30 13:02:58 +0500

    error 404 page added

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="error__section">
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+    name: 'Error Page'
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
